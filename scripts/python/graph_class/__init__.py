13bb22917cb526f5db7b2b4e533666b97092bda8
Albin Printz <albin.printz@gmail.com>
2017-05-19 09:21:17 +0200

    Added saving and loading graph instances functionality via self.save_to_file and self.load_from_file.
    Added distance and conneted sub graphs counting methods.

--- scripts/python/graph_class/__init__.py
+++ scripts/python/graph_class/__init__.py
@@ -2,6 +2,7 @@ class Graph:
     """
     Main graph object.
     """
+
     def __init__(self):
         self._nodes = {}
         self._number_of_subgraphs = None
@@ -14,7 +15,7 @@ class Graph:
         """
         self._nodes[name] = Node(name, length)
 
-    def connect(self, nodeA: str, sectionA: tuple, nodeB: str, sectionB: tuple, 
+    def connect(self, nodeA: str, sectionA: tuple, nodeB: str, sectionB: tuple,
                 similarity: str, lengthA: int, lengthB: int):
         """
         Connects two nodes in the graph. If any of them is not already in the graph, it creates them first.
@@ -48,15 +49,89 @@ class Graph:
 
     def set_number_of_subgraphs(self, number):
         self._number_of_subgraphs = number
-        
+
     def get_number_of_subgraphs(self):
         return self._number_of_subgraphs
+
+    def distance(self, start):
+        """
+        Calculates distance from the node start to all other nodes in the graph.
+        In the returned dictionary, start node gets a distance of zero, 
+        and nodes not connected to the start node gets math.inf.
+        :param start: The node from which to calculate distances.
+        :return: A dictionary like {'node': distance to start, ...}.
+        """
+        import queue
+        import math
+        infty = math.inf
+        dist_so_far = 0
+        dist_dict = {}
+        for node in self._nodes:
+            dist_dict[node] = infty
+        q = queue.Queue()
+        q.put(start)
+        dist_dict[start] = dist_so_far
+        while not q.empty():
+            v = q.get()
+            dist_so_far = dist_dict[v] + 1
+            for u in self._nodes[v]:
+                if dist_dict[u] == infty:
+                    q.put(u)
+                    dist_dict[u] = dist_so_far
+        return dist_dict
+
+    def save_to_file(self, directory):
+        """
+        Uses pickle to save nodes and arcs to a file.
+        :param directory: Save directory.
+        """
+        import pickle
+        fileObject = open(directory, 'wb')
+        pickle.dump(self._nodes, fileObject)
+        fileObject.close()
+
+    def load_from_file(self, directory):
+        """
+        Uses pickle to load nodes and arcs from a file.
+        :param directory: Load directory.
+        """
+        import pickle
+        fileObject = open(directory, 'rb')
+        self._nodes = pickle.load(fileObject)
+        fileObject.close()
+
+    def number_of_csg(self):
+        """
+        Calculates the number of different connected sub graphs is the graph.
+        Also updates the attribute self.number_of_subgraphs.
+        :return: a list containing one node from each connected sub graph.
+        """
+        import math
+        backup = dict(self._nodes)
+        c = 0
+        distinct = []
+        while len(self._nodes) > 0:
+            for k in self._nodes:
+                dist = self.distance(k)
+                if not c % 100:
+                    print(len(self._nodes), 'left to check.')
+                for i in dist:
+                    if dist[i] != math.inf and dist[i] != 0:
+                        self._nodes.pop(i)
+                distinct.append(k)
+                self._nodes.pop(k)
+                break
+            c += 1
+        self._nodes = backup
+        self._number_of_subgraphs = len(distinct)
+        return distinct
 
 
 class DiArc:
     """
     A directional arc class.
     """
+
     def __init__(self, sectionSelf: tuple, sectionOther: tuple, similarity: str):
         self.ownLocation = sectionSelf
         self.otherLocation = sectionOther
@@ -67,6 +142,7 @@ class Node:
     """
     Node class.
     """
+
     def __init__(self, name, length):
         self._name = name
         self._neighbours = {}
@@ -107,14 +183,14 @@ class Node:
         :param number: Internal graph number
         """
         self._graph_number = number
-        
+
     def get_graph_number(self):
         """
         Number of which the node belongs to.
         :return: The number of the graph the node belongs to.
         """
         return self._graph_number
-    
+
     def get_neighbours(self):
         """
         Get the neighbour set of the node.
