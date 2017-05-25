68b6bc736590598b0ac31a1c8546c74f7006f5c0
Anton Wallgren <Anton@Antons-MacBook-Pro.local>
2017-05-25 14:31:42 +0200

    Small fix.

--- scripts/bash/create_new_indata.sh
+++ scripts/bash/create_new_indata.sh
@@ -1,5 +1,4 @@
 rm ../../resources/unsocial_contigs.txt
 split -l 16014193 ../../resources/Spruce_fingerprint_2017-03-10_16.48.olp.m4
-parallel python ../python/social_contig_remover.py ::: xaa xab xac xad
-wc -l unsocial_contigs.txt
+parallel python ../python/social_contig_remover.py ../../resources/over_19_neighbours.txt ../../resources/unsocial_contigs.txt ::: xaa xab xac xad
 rm xa*
