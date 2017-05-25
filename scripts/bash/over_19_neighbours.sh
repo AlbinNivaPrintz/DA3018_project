554c402cd5eeb81077e49173a3cf36924b3bff2b
Anton Wallgren <Anton@Antons-MacBook-Pro.local>
2017-05-25 14:52:30 +0200

    Remonved blank row.

--- scripts/bash/over_19_neighbours.sh
+++ scripts/bash/over_19_neighbours.sh
@@ -1,4 +1,3 @@
-
 cut -f 1 ../../resources/Spruce_fingerprint_2017-03-10_16.48.olp.m4 > column_1_occurences.txt
 echo "First column added."
 cut -f 2 ../../resources/Spruce_fingerprint_2017-03-10_16.48.olp.m4 > column_2_occurences.txt
