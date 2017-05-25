62245f5f0b16eeea2539932b46987e702752dcb7
Anton Wallgren <Anton@Antons-MacBook-Pro.local>
2017-05-25 14:36:10 +0200

    Added more command line arguments.

--- scripts/python/social_contig_remover.py
+++ scripts/python/social_contig_remover.py
@@ -1,16 +1,14 @@
 import sys
 
-indata = str(sys.argv[1])
-
 contig_dict = {}
 
-with open('../../resources/over_19_neighbours.txt', 'r') as remove_file:
+with open(str(sys.argv[1]), 'r') as remove_file:
     for line in remove_file:
         line = line[:-1]
         contig_dict[line] = 1
 
-with open('../../resources/unsocial_contigs.txt', 'a') as result:
-    with open(indata, 'r') as indata:
+with open(str(sys.argv[2]), 'a') as result:
+    with open(str(sys.argv[3]), 'r') as indata:
         for line in indata:
             line = line.split()
             if line[0] in contig_dict:
