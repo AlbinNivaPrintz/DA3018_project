298194aec4d854cd780a2228cc7bd2f4b33c5f40
Anton Wallgren <Anton@Antons-MacBook-Pro.local>
2017-05-26 15:25:13 +0200

    Bugfix.

--- scripts/bash/over_n_neighbours.sh
+++ scripts/bash/over_n_neighbours.sh
@@ -5,9 +5,9 @@ rm column_1_occurences.txt 2>/dev/null
 rm column_2_occurences.txt 2>/dev/null
 split -l $((SIZE/4+1)) $FILE
 parallel bash scripts/bash/contigs.sh ::: xaa xab xac xad
+cat column_1_occurences.txt column_2_occurences.txt | sort | uniq -c > resources/contig_occurences.txt
 rm column_1_occurences.txt
 rm column_2_occurences.txt
-cat column_1_occurences.txt column_2_occurences.txt | sort | uniq -c > resources/contig_occurences.txt
 echo "Finished search for all nodes."
 cat resources/contig_occurences.txt | awk -v var=$n '{if ($1>var) print $2}' > resources/more_neighbours_than_$n.txt
 echo "Finished search for all social nodes."
