4548d7f19bffe63bd6e77621c478831844ca8bd4
Anton Wallgren <Anton@Antons-MacBook-Pro.local>
2017-05-26 15:11:17 +0200

    Added line that removes a large unnecessecary file after running.

--- main.sh
+++ main.sh
@@ -6,3 +6,4 @@ time bash scripts/bash/over_n_neighbours.sh $FILE $n
 time bash scripts/bash/create_new_indata.sh $FILE $n
 echo "Finished input preparation."
 python scripts/python/read_and_divide.py $n
+rm resources/unsocial_contigs_over_$n.txt
