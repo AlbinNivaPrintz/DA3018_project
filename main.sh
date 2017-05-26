4e307e3daa2c2db81623c3b09684611801f31ead
Anton Wallgren <Anton@Antons-MacBook-Pro.local>
2017-05-26 15:12:48 +0200

    Removed timing.

--- main.sh
+++ main.sh
@@ -2,8 +2,8 @@ FILE=$1
 n=$2
 
 echo "Initializing preparation of inputfile..."
-time bash scripts/bash/over_n_neighbours.sh $FILE $n
-time bash scripts/bash/create_new_indata.sh $FILE $n
+bash scripts/bash/over_n_neighbours.sh $FILE $n
+bash scripts/bash/create_new_indata.sh $FILE $n
 echo "Finished input preparation."
 python scripts/python/read_and_divide.py $n
 rm resources/unsocial_contigs_over_$n.txt
