4ea37881174f561530aaf17c32f5e487e629122e
Albin Printz <albin.printz@gmail.com>
2017-05-26 15:15:25 +0200

    Changed output messages

--- scripts/bash/create_new_indata.sh
+++ scripts/bash/create_new_indata.sh
@@ -2,6 +2,7 @@ FILE=$1
 n=$2
 SIZE=`wc -l < $1`
 
+echo "Initializing writing of new input file"
 rm resources/unsocial_contigs_over_$n.txt 2>/dev/null
 parallel python scripts/python/social_contig_remover.py resources/more_neighbours_than_$n.txt resources/unsocial_contigs_over_$n.txt ::: xaa xab xac xad
 rm xa*
--- scripts/bash/over_n_neighbours.sh
+++ scripts/bash/over_n_neighbours.sh
@@ -4,10 +4,10 @@ SIZE=`wc -l < $1`
 rm column_1_occurences.txt 2>/dev/null
 rm column_2_occurences.txt 2>/dev/null
 split -l $((SIZE/4+1)) $FILE
-time parallel bash scripts/bash/contigs.sh ::: xaa xab xac xad
+parallel bash scripts/bash/contigs.sh ::: xaa xab xac xad
 rm column_1_occurences.txt
 rm column_2_occurences.txt
 cat column_1_occurences.txt column_2_occurences.txt | sort | uniq -c > resources/contig_occurences.txt
-echo "Sorted."
+echo "All nodes identified."
 cat resources/contig_occurences.txt | awk -v var=$n '{if ($1>var) print $2}' > resources/more_neighbours_than_$n.txt
-echo "Social nodes found."
+echo "All social nodes identified."
