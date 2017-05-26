b5b99a8a54a9ef66d438e74ef39234ff7be3089a
Anton Wallgren <Anton@Antons-MacBook-Pro.local>
2017-05-26 17:52:24 +0200

    Added skip argument.

--- main.sh
+++ main.sh
@@ -1,8 +1,9 @@
 FILE=$1
 n=$2
+SKIP=$3
 
 echo "Initializing preparation of inputfile..."
-bash scripts/bash/over_n_neighbours.sh $FILE $n
+bash scripts/bash/over_n_neighbours.sh $FILE $n $SKIP
 bash scripts/bash/create_new_indata.sh $FILE $n
 echo "Finished input preparation."
 python scripts/python/read_and_divide.py $n
--- scripts/bash/over_n_neighbours.sh
+++ scripts/bash/over_n_neighbours.sh
@@ -1,13 +1,14 @@
 FILE=$1
 n=$2
+SKIP=$3
 SIZE=`wc -l < $1`
 rm column_1_occurences.txt 2>/dev/null
 rm column_2_occurences.txt 2>/dev/null
 split -l $((SIZE/4+1)) $FILE
-parallel bash scripts/bash/contigs.sh ::: xaa xab xac xad
-cat column_1_occurences.txt column_2_occurences.txt | sort | uniq -c > resources/contig_occurences.txt
-rm column_1_occurences.txt
-rm column_2_occurences.txt
+[ -z "$SKIP" ] && parallel bash scripts/bash/contigs.sh ::: xaa xab xac xad
+[ -z "$SKIP" ] && cat column_1_occurences.txt column_2_occurences.txt | sort | uniq -c > resources/contig_occurences.txt
+[ -z "$SKIP" ] && rm column_1_occurences.txt
+[ -z "$SKIP" ] && rm column_2_occurences.txt
 echo "Finished search for all nodes."
 cat resources/contig_occurences.txt | awk -v var=$n '{if ($1>var) print $2}' > resources/more_neighbours_than_$n.txt
 echo "Finished search for all social nodes."
