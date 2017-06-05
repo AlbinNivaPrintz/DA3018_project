abab78ea641cf6cff419122954137b4b14102809
Anton Wallgren <Anton@Antons-MacBook-Pro.local>
2017-06-05 17:38:36 +0200

    Added bash script to remove doubles and made it work together with the other scripts.

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ SKIP=$3
 
 echo "Initializing preparation of inputfile..."
 bash scripts/bash/over_n_neighbours.sh $FILE $n $SKIP
-bash scripts/bash/create_new_indata.sh $FILE $n
+bash scripts/bash/create_new_indata.sh $n
 echo "Finished input preparation."
 python scripts/python/read_and_divide.py $n
 rm resources/unsocial_contigs_over_$n.txt
